141be1f19f8a980ef296457f3110df880520f291
Marco Silveri <marcantonio1963@libero.it>
2024-11-26 17:12:21 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BestPracticeDirectory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
